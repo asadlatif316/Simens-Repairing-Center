a13be968b9ca94b165c3a1beeb7bb424e5895587
Asad <hellohello51981@gmail.com>
2026-05-30 09:54:00 +0500

    feat: add container component

---
+++ components/global/container.tsx
@@ -0,0 +1,17 @@
+import { cn } from '@/lib/utils';
+
+const Container = ({
+  children,
+  className,
+}: {
+  children: React.ReactNode;
+  className: string;
+}) => {
+  return (
+    <div className={cn('mx-auto max-w-6xl xl:w-7xl px-8', className)}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
